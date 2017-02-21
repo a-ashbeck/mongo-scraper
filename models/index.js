2db16c8779993e204b696bca4f1e7f549c530a8e
Alan <a-ashbeck@northwestern.edu>
2017-02-20 20:03:53 -0600

    Update schemas

--- models/index.js
+++ models/index.js
@@ -3,21 +3,44 @@ var Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://heroku_6p4k868n:m8omduel4nqlonrefu044pfh2t@ds157349.mlab.com:57349/heroku_6p4k868n');
 
-var story = new Schema({
-    title: String,
-    url: String,
+var StorySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+        unique: true
+    },
     timestamp: {
         type: Date,
         default: Date.now
     },
     comments: [{
-        usedId: String,
-        body: String,
-        timestamp: {
-            type: Date,
-            default: Date.now
-        }
+        type: Schema.ObjectId,
+        ref: 'Comments'
     }]
 });
+
+var CommentSchema = new Schema({
+    _story: {
+        type: string,
+        ref: 'Story',
+        required: true
+    },
+    userID: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
  
-module.exports = mongoose.model('story', story);
+module.exports = mongoose.model('Story', StorySchema);
