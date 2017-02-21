0a7f4a39a722c8bb0208620a8a317c8e258998e8
Alan <a-ashbeck@northwestern.edu>
2017-02-20 20:17:39 -0600

    Move connection to server.js

--- models/Story.model.js
+++ models/Story.model.js
@@ -1,8 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-mongoose.connect('mongodb://heroku_6p4k868n:m8omduel4nqlonrefu044pfh2t@ds157349.mlab.com:57349/heroku_6p4k868n');
-
 var StorySchema = new Schema({
     title: {
         type: String,
--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 // Dependencies
-var express = require("express");
-var handlebars = require("express-handlebars");
-var mongojs = require("mongojs");
-var mongoose = require("mongoose");
-var request = require("request");
-var cheerio = require("cheerio");
+var express = require('express');
+var handlebars = require('express-handlebars');
+var mongojs = require('mongojs');
+var mongoose = require('mongoose');
+var request = require('request');
+var cheerio = require('cheerio');
+var Story = require('./models/Story.model')
 
 // Initialize Express
 var app = express();
 
 // Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var databaseUrl = 'scraper';
+var collections = ['scrapedData'];
+
+// Mongoose database connection with Heroku configuration
+mongoose.connect('mongodb://heroku_6p4k868n:m8omduel4nqlonrefu044pfh2t@ds157349.mlab.com:57349/heroku_6p4k868n');
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
+db.on('error', function(error) {
+  console.log('Database Error:', error);
 });
 
 // Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
+app.get('/', function(req, res) {
+  res.send('Hello world');
 });
 
 // Retrieve data from the db
-app.get("/all", function(req, res) {
+app.get('/all', function(req, res) {
   // Find all results from the scrapedData collection in the db
   db.scrapedData.find({}, function(error, found) {
     // Throw any errors to the console
@@ -40,17 +44,17 @@ app.get("/all", function(req, res) {
 });
 
 // Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
+app.get('/scrape', function(req, res) {
   // Make a request for the news section of ycombinator
-  request("https://news.ycombinator.com/", function(error, response, html) {
+  request('https://news.ycombinator.com/', function(error, response, html) {
     // Load the html body from request into cheerio
     var $ = cheerio.load(html);
-    // For each element with a "title" class
-    $(".title").each(function(i, element) {
+    // For each element with a 'title' class
+    $('.title').each(function(i, element) {
       // Save the text of each link enclosed in the current element
-      var title = $(this).children("a").text();
+      var title = $(this).children('a').text();
       // Save the href value of each link enclosed in the current element
-      var link = $(this).children("a").attr("href");
+      var link = $(this).children('a').attr('href'');
 
       // If this title element had both a title and a link
       if (title && link) {
@@ -75,12 +79,12 @@ app.get("/scrape", function(req, res) {
     });
   });
 
-  // This will send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
+  // This will send a 'Scrape Complete' message to the browser
+  res.send('Scrape Complete');
 });
 
 
 // Listen on port 3000
 app.listen(3000, function() {
-  console.log("App running on port 3000!");
+  console.log('App running on port 3000!');
 });
